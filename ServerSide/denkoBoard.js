b112d5802e321bb0d83079e2d6ed23833488bf2e
unknown <nwilson@ams-nwilson2.MSC.local>
2015-06-08 13:39:50 -0500

    Initial commit

---
+++ ServerSide/denkoBoard.js
@@ -0,0 +1,30 @@
+/**
+ * Created by nwilson on 6/8/2015.
+ */
+//Link dependencies
+var express = require('express');
+
+//Setup server
+var app = express();
+var port = 1337;
+app.use(express.static('../ClientSide/'));
+
+//Start web server
+var server = app.listen(port, function () {
+    console.log('denkoBoard server running on port ' + port);
+});
+
+/*
+ Websocket stuff
+ */
+
+//Start websocket server
+var io = require('socket.io').listen(server);
+
+//Socket routes
+io.on('connection', function (socket) {
+
+    socket.on('disconnect', function () {
+
+    });
+});
