989d81afcba1f8f199d0c8dfed8130d079418889
nwilson <nawilson9@gmail.com>
2015-06-09 15:11:17 -0500

    Angular is still trash

--- ClientSide/JS/denkoAngular.js
+++ ClientSide/JS/denkoAngular.js
@@ -1,62 +1,61 @@
 /**
  * Created by Nick on 6/8/2015.
  */
-(function(){
-    var app = angular.module('denkoAngular', []);
-    app.controller('musicController', function(){
-        this.songs = songs;
-    });
+var app = angular.module('denkoAngular', []);
 
-    app.controller('weatherController', function(){
-        this.weather = weather;
-        socket.on('updateWeather', function(data){
-            console.log(JSON.stringify(data));
-            this.weather = false;
-        })
-    });
+app.controller('musicController', function(){
+    this.songs = songs;
+});
 
-    app.controller('infoController', function(){
-        this.announcements = announcements;
-        this.contactInformation = contactInformation;
-    });
+app.controller('weatherController', ['$scope', function($scope){
+    $scope.weather = weather;
+    socket.on('updateWeather', function(data){
+        console.log(JSON.stringify(data));
+        $scope.weather = false;
+    })
+}]);
 
-    var songs = [
-        { title: 'Summer', artist: 'Calvin Harris', albumArt: ''},
-        { title: 'Detonate', artist: 'Netsky', albumArt: ''},
-        { title: 'Fragile Earth', artist: 'UNQUOTE', albumArt: ''},
-        { title: 'Green Destiny', artist: 'Fred V & Grafix', albumArt: ''},
-        { title: 'Song in the Key of Knife', artist: 'London Elektricity', albumArt: ''},
-        { title: 'Black Cloud', artist: 'Royalston', albumArt: ''},
-        { title: 'Detroit', artist: 'Rockwell', albumArt: ''},
-        { title: '1234', artist: 'Rockwell', albumArt: ''},
-        { title: 'INeedU', artist: 'Rockwell', albumArt: ''},
-        { title: 'Atlantis', artist: 'Enei', albumArt: ''}
-    ];
+app.controller('infoController', function(){
+    this.announcements = announcements;
+    this.contactInformation = contactInformation;
+});
 
-    var weather = [
-        { time: '8:00AM', temperature: '72F', conditions: 'Sunny', conditionsImage: ''},
-        { time: '9:00AM', temperature: '72F', conditions: 'Sunny', conditionsImage: ''},
-        { time: '10:00AM', temperature: '73F', conditions: 'Sunny', conditionsImage: ''},
-        { time: '11:00AM', temperature: '73F', conditions: 'Sunny', conditionsImage: ''},
-        { time: '12:00PM', temperature: '74F', conditions: 'Sunny', conditionsImage: ''},
-        { time: '1:00PM', temperature: '74F', conditions: 'Sunny', conditionsImage: ''},
-        { time: '2:00PM', temperature: '75F', conditions: 'Sunny', conditionsImage: ''},
-        { time: '3:00PM', temperature: '74F', conditions: 'Sunny', conditionsImage: ''},
-        { time: '4:00PM', temperature: '73F', conditions: 'Sunny', conditionsImage: ''},
-        { time: '5:00PM', temperature: '72F', conditions: 'Sunny', conditionsImage: ''}
-    ];
+var songs = [
+    { title: 'Summer', artist: 'Calvin Harris', albumArt: ''},
+    { title: 'Detonate', artist: 'Netsky', albumArt: ''},
+    { title: 'Fragile Earth', artist: 'UNQUOTE', albumArt: ''},
+    { title: 'Green Destiny', artist: 'Fred V & Grafix', albumArt: ''},
+    { title: 'Song in the Key of Knife', artist: 'London Elektricity', albumArt: ''},
+    { title: 'Black Cloud', artist: 'Royalston', albumArt: ''},
+    { title: 'Detroit', artist: 'Rockwell', albumArt: ''},
+    { title: '1234', artist: 'Rockwell', albumArt: ''},
+    { title: 'INeedU', artist: 'Rockwell', albumArt: ''},
+    { title: 'Atlantis', artist: 'Enei', albumArt: ''}
+];
 
-    var announcements = [
-        { title: 'Time', comment: 'Get your time in by Friday'},
-        { title: 'OOO', comment: "I'll be OOO Wednesday - Friday. Skype me if you have any problems"},
-        { title: 'Pizza', comment: 'Pizza today!'}
-    ];
+var weather = [
+    { time: '8:00AM', temperature: '72F', conditions: 'Sunny', conditionsImage: ''},
+    { time: '9:00AM', temperature: '72F', conditions: 'Sunny', conditionsImage: ''},
+    { time: '10:00AM', temperature: '73F', conditions: 'Sunny', conditionsImage: ''},
+    { time: '11:00AM', temperature: '73F', conditions: 'Sunny', conditionsImage: ''},
+    { time: '12:00PM', temperature: '74F', conditions: 'Sunny', conditionsImage: ''},
+    { time: '1:00PM', temperature: '74F', conditions: 'Sunny', conditionsImage: ''},
+    { time: '2:00PM', temperature: '75F', conditions: 'Sunny', conditionsImage: ''},
+    { time: '3:00PM', temperature: '74F', conditions: 'Sunny', conditionsImage: ''},
+    { time: '4:00PM', temperature: '73F', conditions: 'Sunny', conditionsImage: ''},
+    { time: '5:00PM', temperature: '72F', conditions: 'Sunny', conditionsImage: ''}
+];
 
-    var contactInformation = [
-        { type: '4 Help Number', value: '123-456-7890'},
-        { type: 'Office Number', value: '123-456-7890'},
-        { type: 'Cell Number', value: '123-456-7890'},
-        { type: 'Email Address', value: 'Bossman@email.com'},
-        { type: 'Skype Name', value: 'Boss.man'}
-    ];
-})();
+var announcements = [
+    { title: 'Time', comment: 'Get your time in by Friday'},
+    { title: 'OOO', comment: "I'll be OOO Wednesday - Friday. Skype me if you have any problems"},
+    { title: 'Pizza', comment: 'Pizza today!'}
+];
+
+var contactInformation = [
+    { type: '4 Help Number', value: '123-456-7890'},
+    { type: 'Office Number', value: '123-456-7890'},
+    { type: 'Cell Number', value: '123-456-7890'},
+    { type: 'Email Address', value: 'Bossman@email.com'},
+    { type: 'Skype Name', value: 'Boss.man'}
+];
