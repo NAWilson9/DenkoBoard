59cab3e99ab5e54694cce18f82f0e34f2fdb9738
Nick <nawilson9@gmail.com>
2015-06-08 22:50:29 -0500

    Baseline Angular Setup

--- ClientSide/JS/denkoAngular.js
+++ ClientSide/JS/denkoAngular.js
@@ -17,30 +17,29 @@
     });
 
     var songs = [
-        { title: 'Summer', artist: 'Calvin Harris'},
-        { title: 'Detonate', artist: 'Netsky'},
-        { title: 'Fragile Earth', artist: 'UNQUOTE'},
-        { title: 'Green Destiny', artist: 'Fred V & Grafix'},
-        { title: 'Song in the Key of Knife', artist: 'London Elektricity'},
-        { title: 'Black Cloud', artist: 'Royalston'},
-        { title: 'Detroit', artist: 'Rockwell'},
-        { title: '1234', artist: 'Rockwell'},
-        { title: 'INeedU', artist: 'Rockwell'},
-        { title: 'Atlantis', artist: 'Enei'}
+        { title: 'Summer', artist: 'Calvin Harris', albumArt: ''},
+        { title: 'Detonate', artist: 'Netsky', albumArt: ''},
+        { title: 'Fragile Earth', artist: 'UNQUOTE', albumArt: ''},
+        { title: 'Green Destiny', artist: 'Fred V & Grafix', albumArt: ''},
+        { title: 'Song in the Key of Knife', artist: 'London Elektricity', albumArt: ''},
+        { title: 'Black Cloud', artist: 'Royalston', albumArt: ''},
+        { title: 'Detroit', artist: 'Rockwell', albumArt: ''},
+        { title: '1234', artist: 'Rockwell', albumArt: ''},
+        { title: 'INeedU', artist: 'Rockwell', albumArt: ''},
+        { title: 'Atlantis', artist: 'Enei', albumArt: ''}
     ];
 
     var weather = [
-        { time: '8:00AM', temperature: '72F', conditions:'Sunny'},
-        { time: '9:00AM', temperature: '72F', conditions:'Sunny'},
-        { time: '10:00AM', temperature: '73F', conditions:'Sunny'},
-        { time: '11:00AM', temperature: '73F', conditions:'Sunny'},
-        { time: '12:00PM', temperature: '74F', conditions:'Sunny'},
-        { time: '1:00PM', temperature: '74F', conditions:'Sunny'},
-        { time: '2:00PM', temperature: '75F', conditions:'Sunny'},
-        { time: '3:00PM', temperature: '74F', conditions:'Sunny'},
-        { time: '4:00PM', temperature: '73F', conditions:'Sunny'},
-        { time: '5:00PM', temperature: '72F', conditions:'Sunny'}
-
+        { time: '8:00AM', temperature: '72F', conditions: 'Sunny', conditionsImage: ''},
+        { time: '9:00AM', temperature: '72F', conditions: 'Sunny', conditionsImage: ''},
+        { time: '10:00AM', temperature: '73F', conditions: 'Sunny', conditionsImage: ''},
+        { time: '11:00AM', temperature: '73F', conditions: 'Sunny', conditionsImage: ''},
+        { time: '12:00PM', temperature: '74F', conditions: 'Sunny', conditionsImage: ''},
+        { time: '1:00PM', temperature: '74F', conditions: 'Sunny', conditionsImage: ''},
+        { time: '2:00PM', temperature: '75F', conditions: 'Sunny', conditionsImage: ''},
+        { time: '3:00PM', temperature: '74F', conditions: 'Sunny', conditionsImage: ''},
+        { time: '4:00PM', temperature: '73F', conditions: 'Sunny', conditionsImage: ''},
+        { time: '5:00PM', temperature: '72F', conditions: 'Sunny', conditionsImage: ''}
     ];
 
     var announcements = [
